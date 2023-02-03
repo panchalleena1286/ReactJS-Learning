afc40abc7dd99973cafe2809bf6312c14ccd7180
panchalleena1286 <panchalleena8839@gmail.com>
2023-02-03 15:24:03 +0530

    initial commit

--- codewith-react/src/App.js
+++ codewith-react/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Greet from './components/Greet';
+import Welcome from './components/welcome';
 
 function App() {
   return (
     <div className="App">
-      <Greet/>
+      <Greet />
+      <Welcome />
     </div>
   );
 }
