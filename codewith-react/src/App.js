58beddfa81b99152c0aa032157784cea8c779693
panchalleena1286 <panchalleena8839@gmail.com>
2023-02-07 14:15:04 +0530

    initial commit

--- codewith-react/src/App.js
+++ codewith-react/src/App.js
@@ -2,14 +2,31 @@
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/welcome';
+import Hello from './components/Hello';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <Greet />
-      <Welcome />
+  // With JSX
+   return (
+   <div className="App" >
+      <Greet name="Bruce" heroName="Batman">
+        <p>the first Greet method</p>
+      </Greet>
+      <Greet name="clark" heroName="superman">
+        <button>Action</button>
+      </Greet>
+      <Greet name="Diana" heroName="wonder women"/>
+
+       <Welcome name="Bruce" heroName="Batman"/>
+       <Welcome name="clark" heroName="superman"/>
+       <Welcome name="Diana" heroName="wonder women"/>
+
+      {/* <Hello /> */} 
     </div>
-  );
+   );
+
+  //Without JSX
+  return React.createElement('div', null, React.createElement('h1', null, 'Hello Leena'))
 }
 
 export default App;
--- codewith-react/src/components/Greet.js
+++ codewith-react/src/components/Greet.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-// function Greet() {
-//     return <h1>Hello Leena</h1>
-// }
-
-const Greet = () => <h1>Hello Leena</h1>
-
+const Greet = props => {
+return(
+    <div>
+        <h1>Hello {props.name} {props.heroName}</h1>
+        {props.children}
+    </div>
+)
+}
 export default Greet;
