824f94e37fd077dd837434e0e04096d1dc605023
panchalleena1286 <panchalleena8839@gmail.com>
2023-02-13 12:29:34 +0530

    index as key

--- codewith-react/src/components/KeyExample.js
+++ codewith-react/src/components/KeyExample.js
@@ -1,15 +1,32 @@
 import React from "react";
 
-const items = ['item 1', 'item 2', 'item 3'];
+// const items = ['item 1', 'item 2', 'item 3'];
+
+// function KeyExample() {
+//   return (
+//     <ul>
+//       {items.map((item, index) => (
+//         <li key={index}>{item}</li>
+//       ))}
+//     </ul>
+//   );
+// }
+
+const items = [
+  {id: 1, name: 'leena 1'},
+  {id: 2, name: 'rachu 2'},
+  {id: 3, name: 'jk 3'},
+];
 
 function KeyExample() {
   return (
     <ul>
-      {items.map((item, index) => (
-        <li key={index}>{item}</li>
+      {items.map(item => (
+        <li key={item.id}>{item.name}</li>
       ))}
     </ul>
   );
 }
+
 
 export default KeyExample;
